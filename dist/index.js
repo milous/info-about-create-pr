a3bd65c55172fb4dbfbec21cd2224f93c7b5d9ef
Milan Otáhal <su@milous.cz>
2021-08-09 12:13:26 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup!

--- dist/index.js
+++ dist/index.js
@@ -12,8 +12,10 @@ async function run() {
 	try {
 		const inputs = {
 			token: core.getInput('repo-token', {required: true}),
-			prAdditional: core.getInput('pr-additional'),
-			issueAdditional: core.getInput('issue-additional'),
+			prBodyPrefix: core.getInput('pr-body-prefix'),
+			prBodySuffix: core.getInput('pr-body-suffix'),
+			issueBodyPrefix: core.getInput('issue-body-prefix'),
+			issueBodySuffix: core.getInput('issue-body-suffix'),
 		}
 
 		const octokit = new github.getOctokit(inputs.token);
@@ -21,50 +23,61 @@ async function run() {
 
 		let headBranch = github.context.payload.pull_request.head.ref;
 		let detectIssueNumberMatch = headBranch.match(/\d+/g);
-		core.info(`Issue additional: ${inputs.issueAdditional}`);
 
-		if (detectIssueNumberMatch !== null) {
+		if (detectIssueNumberMatch === null) {
+			core.debug(`Issue number was not detected from the branch name: ${headBranch}`);
+		} else if (inputs.issueBodyPrefix === "" && inputs.issueBodyPrefix === "") {
+			core.debug('Issue prefix and suffix is not set');
+		} else {
 			let detectIssueNumber = detectIssueNumberMatch[0];
 			core.info(`Detected issue number: ${detectIssueNumber}`);
 
+			const issueResponse = await octokit
+				.request(`GET /repos/${repo}/issues/${detectIssueNumber}`)
+				.then(function (res) {
+					let issueBody = res.data.body;
 
-			if (inputs.issueAdditional !== "") {
-				// const issueRequest = {
-				// 	owner: github.context.repo.owner,
-				// 	repo: github.context.repo.repo,
-				// 	pull_number: github.context.payload.pull_request.number,
-				// }
-				//
-				// issueRequest.body =
-				//
-				// const issueResponse = await octokit.pulls.update(issueRequest);
-				//
-				// core.info(`Response: ${issueResponse.status}`);
-				// if (response.status !== 200) {
-				// 	core.error('Updating the pull request has failed');
-				// }
+					if (inputs.issueBodyPrefix !== "") {
+						issueBody = inputs.issueBodyPrefix + "\n" + issueBody;
+					}
 
-				const issueResponse = await octokit
-					.request(`GET /repos/${repo}/issues/${detectIssueNumber}`)
-					.then(function (res) {
-						let issueBody = res.data.body;
+					if (inputs.issueBodySuffix !== "") {
+						issueBody += "\n" + inputs.issueBodySuffix;
+					}
 
-						issueBody += "\n" + inputs.issueAdditional;
+					return octokit.request(`PATCH /repos/${repo}/issues/${detectIssueNumber}`, {
+						body: issueBody,
+					});
+				})
+			;
 
-						core.info(`New issue body: ${issueBody}`)
-
-						return octokit.request(`PATCH /repos/${repo}/issues/${detectIssueNumber}`, {
-							body: issueBody,
-						});
-					})
-				;
-
-				core.info(`Response: ${issueResponse.status}`);
+			core.info(`Response: ${issueResponse.status}`);
+			if (issueResponse.status !== 200) {
+				core.error('Issue body was not updated');
 			}
 		}
 
-		core.info(`Issue additional text: ${inputs.issueAdditional}`);
-		core.info(`PR additional text: ${inputs.prAdditional}`);
+		if (inputs.prBodyPrefix !== "" || inputs.prBodySuffix !== "") {
+			let prBody = github.context.payload.body
+			if (inputs.prBodyPrefix !== "") {
+				prBody = prBody + "\n" + inputs.prBodyPrefix;
+			}
+
+			if (inputs.prBodySuffix !== "") {
+				prBody += "\n" + inputs.prBodySuffix;
+			}
+
+			const prResponse = await octokit
+				.request(`PATCH /repos/${repo}/issues/${detectIssueNumber}`, {
+					body: prBody,
+				})
+			;
+
+			core.info(`Response: ${prResponse.status}`);
+			if (prResponse.status !== 200) {
+				core.error('PR body was not updated');
+			}
+		}
 	}
 	catch (error) {
 		core.error(error);
